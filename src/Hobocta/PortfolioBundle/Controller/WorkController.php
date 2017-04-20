08dbaa4b27fe39e06bd31c4aea485bf6b6ba9bbc
Антон Кондратов <contact@site-home.ru>
2017-04-20 23:06:51 +0300

    link work show to database

--- src/Hobocta/PortfolioBundle/Controller/WorkController.php
+++ src/Hobocta/PortfolioBundle/Controller/WorkController.php
@@ -8,6 +8,14 @@ class WorkController extends Controller
 {
     public function showAction($code)
     {
-        return $this->render('HoboctaPortfolioBundle:Work:show.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $work = $em->getRepository('HoboctaPortfolioBundle:Work')->findOneBy(array('code' => $code));
+
+        if (!$work) {
+            throw $this->createNotFoundException('Unable to find work');
+        }
+
+        return $this->render('HoboctaPortfolioBundle:Work:show.html.twig', array('work' => $work));
     }
 }
