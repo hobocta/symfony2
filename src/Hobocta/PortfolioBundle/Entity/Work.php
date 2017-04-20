947bd0d5b794b4f426feeae4c907f373fbe69232
Антон Кондратов <contact@site-home.ru>
2017-04-20 22:46:09 +0300

    add work entity

---
+++ src/Hobocta/PortfolioBundle/Entity/Work.php
@@ -0,0 +1,309 @@
+<?php
+
+namespace Hobocta\PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="works")
+ */
+class Work
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $title;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $body;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $details;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $image;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $link;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $code;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $cms;
+
+	/**
+	 * @ORM\Column(type="date")
+	 */
+	protected $date;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $description;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $features;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Work
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Work
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set details
+     *
+     * @param string $details
+     * @return Work
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    /**
+     * Get details
+     *
+     * @return string
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Work
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     * @return Work
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Work
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set cms
+     *
+     * @param string $cms
+     * @return Work
+     */
+    public function setCms($cms)
+    {
+        $this->cms = $cms;
+
+        return $this;
+    }
+
+    /**
+     * Get cms
+     *
+     * @return string
+     */
+    public function getCms()
+    {
+        return $this->cms;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Work
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Work
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set features
+     *
+     * @param string $features
+     * @return Work
+     */
+    public function setFeatures($features)
+    {
+        $this->features = $features;
+
+        return $this;
+    }
+
+    /**
+     * Get features
+     *
+     * @return string
+     */
+    public function getFeatures()
+    {
+        return $this->features;
+    }
+}
