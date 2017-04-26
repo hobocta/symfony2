fbf9c019b6c14bf8ff908e5685ad483bb660af62
Антон Кондратов <contact@site-home.ru>
2017-04-27 00:58:01 +0300

    fos_user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
 
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Hobocta/PortfolioBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hobocta\PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Entity\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+}
